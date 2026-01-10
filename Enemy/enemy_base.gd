f15fbed21b3bb83375a9abee54492fc55aaeb34d
FilipAkaMojzesz <filipakamojzesz@gmail.com>
2026-01-10 15:06:41 +0100

    Enemy

---
+++ Enemy/enemy_base.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+class_name Enemy_Class
+
+var speed: float = 500.0
+var step_distance: float = 250.0
+var loudness: int = 10
+var hp: int = 100
+
+@onready var NoiseCollider: CollisionShape2D = $NoiseArea/CollisionShape2D
+@onready var noise_area: Area2D = $NoiseArea
+
+
+func initiate(s: float, sd: float, l: int, _hp: int) -> void:
+	speed = s
+	step_distance = sd
+	loudness = l
+	hp = _hp
+	var circle = NoiseCollider.shape as CircleShape2D
+	circle.radius = loudness * 100
+	
+func make_noise() -> void:
+	if noise_area.has_overlapping_bodies():
+		var TowerArray = noise_area.get_overlapping_bodies()
+		for tower as Tower in TowerArray:
+			tower.hear_enemy()
