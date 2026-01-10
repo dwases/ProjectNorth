28393defd4c73877f55b14f8e4b5c849ee4ae5a6
pm53854 <piwkomi@gmail.com>
2026-01-11 00:39:24 +0100

    Dodano samochód - gotowe do implementacji

--- Enemy/Abilities/interact_ability.gd
+++ Enemy/Abilities/interact_ability.gd
@@ -1,23 +1,8 @@
 extends EnemyAbility
 class_name InteractAbility
 
-@export var interactRadius: float = 100
-var interactArea: Area2D = null
-var interactCollision: CollisionShape2D = null
-var interactShape: CircleShape2D = null
-
-func initialize_ability():
-	print("INTERACT INIT")
-	#interactArea = Area2D.new()
-	#interactCollision = CollisionShape2D.new()
-	#interactShape = CircleShape2D.new()
-	#interactShape.radius = interactRadius
-	#interactCollision.shape = interactShape
-	#interactArea.collision_layer = 0
-	#interactArea.collision_mask = 3
-	#interactArea.add_child(interactCollision)
-	
-	print("INTERACT INIT END")
+func initialize_ability(enemy: Node2D):
+	enemy.can_interact = true
 
 func interact(otherObj: Node2D):
 	if otherObj.has_method("interact"):
