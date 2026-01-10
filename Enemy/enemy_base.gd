b578c89f274a5acf38df5ea27129b68243151b63
pm53854 <piwkomi@gmail.com>
2026-01-10 19:18:03 +0100

    Dodano statystyki wrogów

--- Enemy/enemy_base.gd
+++ Enemy/enemy_base.gd
@@ -1,22 +1,15 @@
 extends CharacterBody2D
 class_name Enemy_Class
 
-var speed: float = 500.0
-var step_distance: float = 250.0
-var loudness: int = 10
-var hp: float = 100
+@export var stats: EnemyStats
 
 @onready var NoiseCollider: CollisionShape2D = $NoiseArea/CollisionShape2D
 @onready var noise_area: Area2D = $NoiseArea
 
 
-func initiate(s: float, sd: float, l: int, _hp: float) -> void:
-	speed = s
-	step_distance = sd
-	loudness = l
-	hp = _hp
+func initiate() -> void:
 	var circle = NoiseCollider.shape as CircleShape2D
-	circle.radius = l * 100
+	circle.radius = stats.loudness * 100
 	
 func make_noise() -> void:
 	if noise_area.has_overlapping_bodies():
@@ -25,8 +18,8 @@ func make_noise() -> void:
 			tower.hear_enemy(self)
 
 func take_damage(amount: float) -> void:
-	hp -= amount
-	if hp <= 0:
+	stats.HP -= amount
+	if stats.HP <= 0:
 		get_parent().queue_free()
 	
 	
