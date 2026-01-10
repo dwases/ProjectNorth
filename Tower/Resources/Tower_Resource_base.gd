5ca1bb1f0bd49914a899ce104f5696b403f45b3f
PuszczM <162131589+PuszczM@users.noreply.github.com>
2026-01-10 15:06:18 +0100

    początek skryptu i resourców towerów

---
+++ Tower/Resources/Tower_Resource_base.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name TowerStats
+
+@export var Zasieg: int = 0 
+@export var Attack_speed: float = 0
+@export var Damage: float = 0 
+@export var BaseCost: int = 0  
+@export var upgrade_cost: Array[int] = [150, 300, 500]
+@export var UpgradeStructure: bool = true
---
+++ Tower/tower_base.gd
@@ -0,0 +1,37 @@
+extends StaticBody2D
+class_name Tower
+
+var stats: TowerStats
+var targets_in_range: Array[Node2D] = []
+
+func get_best_target() -> Node2D:
+	if targets_in_range.is_empty():
+		return null
+	
+	targets_in_range.sort_custom(func(a, b):
+		if a.loudness != b.loudness:
+			return a.loudness > b.loudness
+		return a.progress > b.progress
+	)
+	return targets_in_range[0]
+var can_shoot = true
+
+func _process(delta):
+	var target = get_best_target()
+	if target and can_shoot:
+		shoot(target)
+
+func shoot(target):
+	can_shoot = false
+	$ReloadTimer.wait_time = 1.0 / stats.attack_speed 
+	$ReloadTimer.start()
+
+func _on_reload_timer_timeout():
+	can_shoot = true
+
+func hear_enemy(enemy):
+	if not targets_in_range.has(enemy):
+		targets_in_range.append(enemy)
+	await get_tree().create_timer(1.0).timeout 
+	if is_instance_valid(enemy) and targets_in_range.has(enemy):
+		targets_in_range.erase(enemy)
